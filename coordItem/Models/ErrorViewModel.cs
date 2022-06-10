13668ce5b2f3650a23fb9f524fca8cee0749851b
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-06-10 14:50:18 -0400

    init commit

---
+++ coordItem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coordItem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
